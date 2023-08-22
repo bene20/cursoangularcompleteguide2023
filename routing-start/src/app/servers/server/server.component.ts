48eecf66ff1fae69bf583505ce853f9638f672d4
Ebenézer Rangel Botelho <bene20@gmail.com>
2023-08-22 13:50:50 -0300

    Até aula 139

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -10,10 +11,20 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    this.updateServer(this.route.snapshot.params); 
+    this.route.params.subscribe(
+      (params: Params) => this.updateServer(params)
+    );
+  }
+
+  updateServer(params: Params){
+    // o '+' abaixo converte o string em número
+    const idServer = +params['id']; 
+    this.server = this.serversService.getServer(idServer);
   }
 
 }

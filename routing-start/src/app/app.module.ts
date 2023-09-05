4b3f4125b560de95efbfffc3243cbc221c50abf7
Ebenézer Rangel Botelho <bene20@gmail.com>
2023-09-05 11:36:51 -0300

    Rota para páginas não encontradas - Até aula 144

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent },
@@ -22,6 +22,8 @@ const appRoutes: Routes = [
     {path: ':id', component: ServerComponent },
     {path: ':id/edit', component: EditServerComponent }
   ]},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: '**', redirectTo: '/not-found'}
 ];
 
 @NgModule({

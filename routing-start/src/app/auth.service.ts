6ab278711e0a1e9ff5257924c7b9c1d8cbeceba4
Ebenézer Rangel Botelho <ebenezer.botelho@trt10.jus.br>
2023-09-08 12:26:28 -0300

    Usando o Route Guard - Até aula 147

---
+++ routing-start/src/app/auth.service.ts
@@ -0,0 +1,25 @@
+// Classe fake para simular um service de autenticação no backend
+export class AuthService {
+  loggedIn = false;
+
+  isAuthenticated(){
+    const promise = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          resolve(this.loggedIn)
+        }, 800);
+      }
+    );
+    return promise;
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+
+  logout() {
+    this.loggedIn = false;
+  }
+
+}

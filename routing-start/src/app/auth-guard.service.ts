1b2f28fbfd098e4783e25c5cc1122bcf6fab9c86
Ebenézer Rangel Botelho <ebenezer.botelho@trt10.jus.br>
2023-09-08 12:32:52 -0300

    Protegendo rota filha (child) com o Route Guard - Até aula 148

--- routing-start/src/app/auth-guard.service.ts
+++ routing-start/src/app/auth-guard.service.ts
@@ -1,28 +1,33 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { AuthService } from "./auth.service";
 import { Observable } from "rxjs";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
   constructor(private authService: AuthService,
               private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService
-        .isAuthenticated()
-        .then(
-          (authenticated: boolean) => {
-            if(authenticated) {
-              return true;
-            } else {
-              this.router.navigate(['/']);
-              return false;
-            }
+      .isAuthenticated()
+      .then(
+        (authenticated: boolean) => {
+          if(authenticated) {
+            return true;
+          } else {
+            this.router.navigate(['/']);
+            return false;
           }
-        )
+        }
+      );
 
   }
+
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(route, state);
+  }
+
 
 }
